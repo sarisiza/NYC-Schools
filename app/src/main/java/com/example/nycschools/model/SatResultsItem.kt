1ad60e2adb0f81f75daff9eb0aed1fbbdda1d270
Sara Iza <sasita.iza@gmail.com>
2023-01-31 09:44:12 -0500

    Added SatResultsItem.kt data class

---
+++ app/src/main/java/com/example/nycschools/model/SatResultsItem.kt
@@ -0,0 +1,17 @@
+package com.example.nycschools.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SatResultsItem(
+    @Json(name = "dbn")
+    val dbn: String? = null,
+    @Json(name = "sat_critical_reading_avg_score")
+    val satCriticalReadingAvgScore: String? = null,
+    @Json(name = "sat_math_avg_score")
+    val satMathAvgScore: String? = null,
+    @Json(name = "sat_writing_avg_score")
+    val satWritingAvgScore: String? = null,
+)
