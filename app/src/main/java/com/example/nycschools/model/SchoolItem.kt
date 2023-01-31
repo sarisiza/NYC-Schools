2faa69083228a5dbe44a4f20f38fd76f726a27f7
Sara Iza <sasita.iza@gmail.com>
2023-01-31 09:35:18 -0500

    Updated gradle to add moshi Added SchoolItem.kt data class

---
+++ app/src/main/java/com/example/nycschools/model/SchoolItem.kt
@@ -0,0 +1,27 @@
+package com.example.nycschools.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SchoolItem(
+    @Json(name = "dbn")
+    val dbn: String? = null,
+    @Json(name = "latitude")
+    val latitude: String? = null,
+    @Json(name = "location")
+    val location: String? = null,
+    @Json(name = "longitude")
+    val longitude: String? = null,
+    @Json(name = "overview_paragraph")
+    val overviewParagraph: String? = null,
+    @Json(name = "phone_number")
+    val phoneNumber: String? = null,
+    @Json(name = "school_email")
+    val schoolEmail: String? = null,
+    @Json(name = "school_name")
+    val schoolName: String? = null,
+    @Json(name = "website")
+    val website: String? = null,
+)
